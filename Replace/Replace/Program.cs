7db1a04da6ee4e87c1c1b8b783b39feca4db34c4
SiddheshGadkar06 <79743244+SiddheshGadkar06@users.noreply.github.com>
2024-04-30 13:00:10 +0530

    Add files via upload

---
+++ Replace/Replace/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string input = Console.ReadLine();
+            //string rep = input.Replace('a', '*');
+
+            string[] arr = input.Split(' ');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int index = arr[i].IndexOf('a');
+                if (index != -1)
+                {
+                    arr[i] = arr[i].Remove(index, 1).Insert(index, "*");
+                }
+            }
+
+            string rep = String.Join(" ", arr);
+
+            var b= arr.Where(s => s.Contains('b'));
+            foreach(var s in b)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine(rep);
+            
+            Console.ReadKey();
+        }
+    }
+}
